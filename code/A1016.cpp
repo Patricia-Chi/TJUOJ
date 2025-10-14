ebcdae33af7f5617db4a3ac682b273e54bc44486
Patricia <88025155+Patricia-Chi@users.noreply.github.com>
2025-10-15 01:36:41 +0800

    Add files via upload

---
+++ code/A1016.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define endl '\n'
+// #define int long long // 不开 long long 见祖宗
+//  #pragma GCC optimize(3)
+
+using namespace std;
+
+const double PI = 3.141592653589793;
+int T;
+int n, m, k;
+int ans;
+string s;
+
+void solve()
+{
+    cin >> n >> k;
+    cout << k * n / (k - 1);
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+    T = 1;
+    // cin >> T;
+    while (T--)
+    {
+        solve();
+    }
+    return 0;
+}
