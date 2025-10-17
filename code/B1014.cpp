45d0f31a5a49ba9f347d1335efb2c66a4214384c
Patricia <88025155+Patricia-Chi@users.noreply.github.com>
2025-10-17 11:35:19 +0800

    Add files via upload

---
+++ code/B1014.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define endl '\n'
+// #define int long long // 不开 long long 见祖宗
+//  #pragma GCC optimize(3)
+
+using namespace std;
+
+const double PI = 3.141592653589793;
+int T;
+int k;
+ll ans;
+string s;
+int n;
+
+void solve()
+{
+    cin >> n;
+    vector<int> a(n + 1);
+    a[0] = 1, a[1] = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        a[i] = a[i - 1] + a[i - 2];
+    }
+    cout << a[n];
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+    T = 1;
+    // cin >> T;
+    while (T--)
+    {
+        solve();
+    }
+    return 0;
+}
